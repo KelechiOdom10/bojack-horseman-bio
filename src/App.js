2b2cd272fb326d4304bbafdf3a7192dfc9c32862
KelechiOdom10 <kelechiodom10@gmail.com>
2020-03-25 20:13:08 +0000

    Added an image to my card

--- src/App.js
+++ src/App.js
@@ -7,13 +7,13 @@ import {Card, Button} from 'react-bootstrap'
 function App() {
   return (
     <div className="App">
-      <Card style={{ width: '20rem' }}>
-        <Card.Img variant="top" src="holder.js/100px180" />
+      <Card style={{ width: '20rem', height:'20rem' }}>
+        <Card.Img variant="top" src="../public/bojack.jpg" />
         <Card.Body>
           <Character 
             name = 'Bojack Horseman' 
             actor = 'Will Arnett' 
-            description = 'He is a self-loathing, alcoholic, anthropomorphic horse currently in his 50s. Though he began as a young bright-eyed actor, he has since grown bitter, deeply depressed, and jaded about Hollywoo and who he has become post-fame.' 
+            description = 'A self-loathing, alcoholic, anthropomorphic horse who began as a young bright-eyed actor but has since grown bitter, deeply depressed.' 
             quote = '"I Feel Like My Life Is Just A Series Of Unrelated Wacky Adventures."' />
         </Card.Body>
       </Card>
