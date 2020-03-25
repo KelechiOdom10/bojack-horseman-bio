96f601ab783e60e28b3789c455851d2570e909f1
KelechiOdom10 <kelechiodom10@gmail.com>
2020-03-25 20:02:17 +0000

    Converted the new App.js to a card using the bootstrap feature

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Character from './components/Character.js';
+import {Card, Button} from 'react-bootstrap'
+
 
 function App() {
   return (
     <div className="App">
-      <h2>Bojack Horseman</h2>
-      <p>Voiced by <a href = '#'>Will Garnet</a></p>
-      <p> He is a self-loathing, alcoholic, anthropomorphic horse currently in his 50s. Though he began as a young bright-eyed actor, he has since grown bitter, deeply depressed, and jaded about Hollywoo and who he has become post-fame. </p>
-      <blockquote>"I Feel Like My Life Is Just A Series Of Unrelated Wacky Adventures."</blockquote>
+      <Card style={{ width: '20rem' }}>
+        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Body>
+          <Character 
+            name = 'Bojack Horseman' 
+            actor = 'Will Arnett' 
+            description = 'He is a self-loathing, alcoholic, anthropomorphic horse currently in his 50s. Though he began as a young bright-eyed actor, he has since grown bitter, deeply depressed, and jaded about Hollywoo and who he has become post-fame.' 
+            quote = '"I Feel Like My Life Is Just A Series Of Unrelated Wacky Adventures."' />
+        </Card.Body>
+      </Card>
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,12 +1,14 @@
 import React from 'react'
 
 function Character(props) {
-    <div>
-      <h2>{props.name}</h2>
-      <p>Voiced by <a href = '#'>{props.actor}</a></p>
-      <p>{props.description}</p>
-      <blockquote>props.quote</blockquote>
-    </div>
+    return (
+        <div>
+        <h2>{props.name}</h2>
+        <p className = 'voiced'>Voiced by <a href = '#'>{props.actor}</a></p>
+        <p>{props.description}</p>
+        <blockquote>{props.quote}</blockquote>
+        </div>
+    )
   }
 
   export default Character;
