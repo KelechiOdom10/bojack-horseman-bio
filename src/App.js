0a6e47397c7079a62f1eda6d6c50c14fd3e4f8c8
KelechiOdom10 <kelechiodom10@gmail.com>
2020-03-25 19:32:21 +0000

    Added a basic bio and styled it a bit

--- src/App.js
+++ src/App.js
@@ -5,22 +5,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Bojack Horseman</h2>
+      <p className = 'voiced' >Voiced by <a href = '#'>Will Garnet</a></p>
+      <p> He is a self-loathing, alcoholic, anthropomorphic horse currently in his 50s. Though he began as a young bright-eyed actor, he has since grown bitter, deeply depressed, and jaded about Hollywoo and who he has become post-fame. </p>
+      <blockquote>"I Feel Like My Life Is Just A Series Of Unrelated Wacky Adventures."</blockquote>
     </div>
   );
 }
+
 
 export default App;
