5d63c0235a27cca5eeeb10812d20098c9353ddd7
KelechiOdom10 <kelechiodom10@gmail.com>
2020-03-26 10:06:11 +0000

    Added a new component by abstracting out the actual bio cards to be reusable

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
-import Character from './components/Character.js';
-import {Card, Button} from 'react-bootstrap'
+import Bio from './components/Bio.js'
 
 
 function App() {
@@ -9,49 +8,13 @@ function App() {
     <div className="App">
       <h1>Bojack Horseman Characters</h1>
       <div className = 'list'>
-        <div className = 'box'>
-        <Card style={{ width: '20rem', height: '24rem'}}>
-          <Card.Img variant="top" src='../assets/bojack.jpg' />
-            <Character 
-              name = 'Bojack Horseman' 
-              actor = 'Will Arnett' 
-              description = 'A self-loathing, alcoholic, anthropomorphic horse.' 
-              quote = '"I Feel Like My Life Is Just A Series Of Unrelated Wacky Adventures."' />
-        </Card>
-        </div>
-
-        <div className = 'box'> 
-        <Card style={{ width: '20rem', height: '24rem'}}>
-          <Card.Img variant="top" src='../assets/diane.jpg' />
-            <Character 
-              name = 'Diane Nguyen' 
-              actor = 'Alison Brie' 
-              description = 'A Vietnamese-American third-wave feminist from Boston' 
-              quote = '“Sometimes Life’s A B**** And Then You Keep Living.”' />
-        </Card>
-        </div>
-
-        <div className = 'box'> 
-        <Card style={{ width: '20rem', height: '24rem'}}>
-          <Card.Img variant="top" src='../assets/peanutbutter.jpg' />
-            <Character 
-              name = 'Mr. Peanutbutter' 
-              actor = 'Paul F. Tompkins' 
-              description = 'An energetic and cheerful yellow Labrador Retrieve' 
-              quote = '“The universe is a cruel, uncaring void...eventually, you’ll be dead.”' />
-        </Card>
-        </div>
-
-        <div className = 'box'> 
-        <Card style={{ width: '20rem', height: '24rem'}}>
-          <Card.Img variant="top" src='../assets/bojack.jpg' />
-            <Character 
-              name = 'Bojack Horseman' 
-              actor = 'Will Arnett' 
-              description = 'A self-loathing, alcoholic, anthropomorphic horse.' 
-              quote = '“The universe is a cruel, uncaring void...eventually, you’ll be dead.”' />
-        </Card>
-        </div>
+        <Bio 
+          link = '/assets/bojack.jpg'
+          name = 'Bojack Horseman' 
+          actor = 'Will Arnett' 
+          description = 'A self-loathing, alcoholic, anthropomorphic horse.' 
+          quote = '"I Feel Like My Life Is Just A Series Of Unrelated Wacky Adventures."' 
+        />
       </div>
     </div>
   );
---
+++ src/components/Bio.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Card, Button} from 'react-bootstrap';
+import Character from '../components/Character'
+
+function Bio(props) {
+    return (
+        <div className = 'box'>
+            <Card style={{ width: '20rem', height: '24rem'}}>
+            <Card.Img variant="top" src={props.link} />
+                <Character 
+                name = {props.name} 
+                actor = {props.actor} 
+                description = {props.description} 
+                quote = {props.quote} />
+            </Card>
+        </div>
+    )
+}
+
+export default Bio
